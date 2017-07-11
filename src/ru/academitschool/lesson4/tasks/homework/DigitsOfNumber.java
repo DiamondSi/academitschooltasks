5e6782c8ab7dbe4a824b820003874c7f32e09a89
DiamondSi <neighborland@outlook.com>
2017-07-11 23:25:08 +0700

    digits of number task solved

--- src/ru/academitschool/lesson4/tasks/homework/DigitsOfNumber.java
+++ src/ru/academitschool/lesson4/tasks/homework/DigitsOfNumber.java
@@ -3,15 +3,28 @@ package ru.academitschool.lesson4.tasks.homework;
 import java.util.Scanner;
 
 // 4.35
-//TODO доделать задачу
 public class DigitsOfNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter the number: ");
         int number = scanner.nextInt();
+        int sum = 0;
+        int oddSum = 0;
+        int max = 0;
         int i = number;
+
         do {
+            int lastDigit = i % 10;
+            sum += lastDigit;
+            if (lastDigit % 2 != 0) {
+                oddSum += lastDigit;
+            }
+            max = (max > lastDigit) ? max : lastDigit;
             i /= 10;
-        } while (i / 10 > 0);
+        } while (i > 0);
+        System.out.println("sum: " + sum);
+        System.out.println("oddSum: " + oddSum);
+        System.out.println("max: " + max);
     }
 }
