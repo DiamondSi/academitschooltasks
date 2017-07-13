f36db671640bcbc4e7ebcdfe00c13f73d9b68d71
DiamondSi <neighborland@outlook.com>
2017-07-13 15:55:20 +0700

    EuclideanAlgorithm solved

--- src/ru/academitschool/lesson5/tasks/course/EuclideanAlgorithm.java
+++ src/ru/academitschool/lesson5/tasks/course/EuclideanAlgorithm.java
@@ -3,8 +3,18 @@ package ru.academitschool.lesson5.tasks.course;
 // 5.37
 public class EuclideanAlgorithm {
     public static void main(String[] args) {
-        int a=1122;
-        int b=1228;
-        //TODO solve task
+        int firstNumber = 3100;
+        int secondNumber = 4440;
+        int a = firstNumber;
+        int b = secondNumber;
+        while (a != 0 && b != 0) {
+            if (a > b) {
+                a %= b;
+            } else {
+                b %= a;
+            }
+        }
+        int greatestCommonDivisor = a + b;
+        System.out.printf("a: %d, b: %d, Greatest Common Divisor: %d", firstNumber, secondNumber, greatestCommonDivisor);
     }
 }
