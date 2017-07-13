7f2d84c95a436ea46268ad55358b0e7528e07a27
DiamondSi <neighborland@outlook.com>
2017-07-13 07:16:48 +0700

    started lesson.5 course task "Euclidean algorithm"

---
+++ src/ru/academitschool/lesson5/tasks/course/EuclideanAlgorithm.java
@@ -0,0 +1,10 @@
+package ru.academitschool.lesson5.tasks.course;
+
+// 5.37
+public class EuclideanAlgorithm {
+    public static void main(String[] args) {
+        int a=1122;
+        int b=1228;
+        //TODO solve task
+    }
+}
