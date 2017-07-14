679f141d83e1879f139f67ddf88b296c3b8fb04e
DiamondSi <neighborland@outlook.com>
2017-07-14 11:30:14 +0700

    Multiplication table solved

--- src/ru/academitschool/lesson5/tasks/course/MultiplicationTable.java
+++ src/ru/academitschool/lesson5/tasks/course/MultiplicationTable.java
@@ -1,9 +1,24 @@
 package ru.academitschool.lesson5.tasks.course;
 
 // 5.38
-//TODO решить эту задачу
 public class MultiplicationTable {
     public static void main(String[] args) {
+        int number = 10;
 
+        System.out.printf("%4s", "|");
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%4d", i);
+        }
+
+        System.out.printf("%n--------------------------------------------%n");
+
+        for (int i = 1; i <= number; i++) {
+            System.out.printf("%2d |", i);
+            for (int j = 1; j <= 10; j++) {
+                int result = i * j;
+                System.out.printf("%4d", result);
+            }
+            System.out.println();
+        }
     }
 }
