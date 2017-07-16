a652f653dacecab428509b1bad6dc4d29c989e37
DiamondSi <neighborland@outlook.com>
2017-07-16 13:55:39 +0700

    lesson.5 course task "Deposit" fix magic numbers and articles

--- src/ru/academitschool/lesson5/tasks/course/Deposit.java
+++ src/ru/academitschool/lesson5/tasks/course/Deposit.java
@@ -9,29 +9,32 @@ public class Deposit {
 
         System.out.print("Введите сумму первоначального взноса:");
         long primaryPayment = scanner.nextLong();
-        long primaryPaymentCent = primaryPayment * 100;
+
         System.out.print("Введите процентную ставку (%% в год):");
         int interestRate = scanner.nextInt();
         System.out.print("Введите срол депозита (месяцев):");
         int depositTerm = scanner.nextInt();
 
-        final int MONTHS_OF_THE_YEAR = 12;
+        final int MONTHS_OF_YEAR = 12;
+        final int ONE_HOUNDRED_PERCENTS = 100;
+        final int CENTS = 100;
+        long primaryPaymentCent = primaryPayment * CENTS;
         long accountBalance = primaryPaymentCent;
         long incomingBalance = 0;
 
         System.out.printf("           остаток на начало месяца: %10.2f;      первоначальный взнос: %10.2f;" +
-                        " остаток по вкладу: %10.2f%n", (double) incomingBalance, (double) primaryPayment,
-                (double) accountBalance / 100);
+                        " остаток по вкладу: %10.2f%n",
+                (double) incomingBalance / CENTS, (double) primaryPayment, (double) accountBalance / CENTS);
         incomingBalance = accountBalance;
         for (int i = 1; i <= depositTerm; i++) {
-            long interest = incomingBalance * interestRate / 100 / MONTHS_OF_THE_YEAR;
+            long interest = incomingBalance * interestRate / ONE_HOUNDRED_PERCENTS / MONTHS_OF_YEAR;
             accountBalance = incomingBalance + interest;
             System.out.printf("месяц: %2d; остаток на начало месяца: %10.2f; ежемесячная капитализация: %10.2f;" +
-                            " остаток по вкладу: %10.2f%n", i, (double) incomingBalance / 100, (double) interest / 100,
-                    (double) accountBalance / 100);
+                            " остаток по вкладу: %10.2f%n",
+                    i, (double) incomingBalance / CENTS, (double) interest / CENTS, (double) accountBalance / CENTS);
             incomingBalance = accountBalance;
         }
         long profit = accountBalance - primaryPaymentCent;
-        System.out.printf("Прибыль: %10.2f", (double) profit / 100);
+        System.out.printf("Прибыль: %10.2f", (double) profit / CENTS);
     }
 }
