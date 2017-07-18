f90981c93520fb5469fc428fd0986dba6c65b401
DiamondSi <neighborland@outlook.com>
2017-07-19 06:50:50 +0700

    lesson.8 max substring bugfix for 0 figures-string

--- src/ru/academitschool/lesson8/tasks/course/MaxSubstring.java
+++ src/ru/academitschool/lesson8/tasks/course/MaxSubstring.java
@@ -1,24 +1,35 @@
 package ru.academitschool.lesson8.tasks.course;
 
+import java.util.Scanner;
+
 // 8.20
 public class MaxSubstring {
     public static void main(String[] args) {
-        System.out.println(findMaxLenSubstring("0ааадееееееееееееееЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕггггвгггггггг"));
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите строку: ");
+        String string = scanner.nextLine();
+        System.out.printf("Максимальная длина подстроки одинаковых символов, без учета регистра: %d",
+                findMaxLenSubstring(string));
     }
 
     public static int findMaxLenSubstring(String string) {
-        int subStringLength = 1;
-        int subStringLengthMax = 1;
-        for (int i = 1; i < string.length(); i++) {
-            if (Character.toLowerCase(string.charAt(i - 1)) == Character.toLowerCase(string.charAt(i))) {
-                ++subStringLength;
-                if (subStringLength > subStringLengthMax) {
-                    subStringLengthMax = subStringLength;
+        if (string.length() == 0) {
+            return 0;
+        } else {
+            int subStringLength = 1;
+            int subStringLengthMax = 1;
+            for (int i = 1; i < string.length(); i++) {
+                if (Character.toLowerCase(string.charAt(i - 1)) == Character.toLowerCase(string.charAt(i))) {
+                    ++subStringLength;
+                    if (subStringLength > subStringLengthMax) {
+                        subStringLengthMax = subStringLength;
+                    }
+                } else {
+                    subStringLength = 1;
                 }
-            } else {
-                subStringLength = 1;
             }
+            return subStringLengthMax;
         }
-        return subStringLengthMax;
     }
 }
