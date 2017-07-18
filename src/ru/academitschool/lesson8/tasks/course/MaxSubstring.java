6ffa020d879d304174b810bc26a1f0846482b2c6
DiamondSi <neighborland@outlook.com>
2017-07-18 22:14:49 +0700

    lesson.8 max substring

--- src/ru/academitschool/lesson8/tasks/course/MaxSubstring.java
+++ src/ru/academitschool/lesson8/tasks/course/MaxSubstring.java
@@ -1,21 +1,24 @@
 package ru.academitschool.lesson8.tasks.course;
 
 // 8.20
-// TODO доделать задачу
 public class MaxSubstring {
+    public static void main(String[] args) {
+        System.out.println(findMaxLenSubstring("0ааадееееееееееееееЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕггггвгггггггг"));
+    }
+
     public static int findMaxLenSubstring(String string) {
         int subStringLength = 1;
         int subStringLengthMax = 1;
-        for (int i = 0; i < string.length(); i++) {
-            if (string.charAt(i + 1) == string.charAt(i)) {
+        for (int i = 1; i < string.length(); i++) {
+            if (Character.toLowerCase(string.charAt(i - 1)) == Character.toLowerCase(string.charAt(i))) {
                 ++subStringLength;
+                if (subStringLength > subStringLengthMax) {
+                    subStringLengthMax = subStringLength;
+                }
             } else {
-                subStringLength=1;
-            }
-            if (subStringLength > subStringLengthMax) {
-                subStringLengthMax = subStringLength;
                 subStringLength = 1;
             }
         }
+        return subStringLengthMax;
     }
 }
