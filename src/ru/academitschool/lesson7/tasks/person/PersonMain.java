88778b0ae1f27aa85d9b3047a79614cf757205e3
DiamondSi <neighborland@outlook.com>
2017-07-19 23:09:43 +0700

    lesson.7 objects

---
+++ src/ru/academitschool/lesson7/tasks/person/PersonMain.java
@@ -0,0 +1,10 @@
+package ru.academitschool.lesson7.tasks.person;
+
+//import ru.academitschool.lesson7.tasks.person.Contact;
+//
+public class PersonMain {
+    public static void main(String[] args) {
+        Contact person = new Contact("Dubchak", "Dmitry", "+79139180207");
+        Contact person2 = new Contact("Dubchak", "Ivan", "+79139180204");
+    }
+}
