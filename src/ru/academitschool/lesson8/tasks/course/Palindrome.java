84472d8609ddde9d92ac1ebd9effc4dea2ceac5b
DiamondSi <neighborland@outlook.com>
2017-07-24 18:21:51 +0700

    palindrome

--- src/ru/academitschool/lesson8/tasks/course/Palindrome.java
+++ src/ru/academitschool/lesson8/tasks/course/Palindrome.java
@@ -3,12 +3,23 @@ package ru.academitschool.lesson8.tasks.course;
 // 8.21
 public class Palindrome {
     public static void main(String[] args) {
-        System.out.println(isPalindrome("Аргентина манит негра"));
+        String string1 = "Аргентина манит негра.";
+        String string2 = "А, Роза упала на лапу Азора!";
+        String string3 = "Мама мыла раму.";
+        System.out.printf("\"%s\" - is palindrome: %b%n", string1, isPalindrome(string1));
+        System.out.printf("\"%s\" - is palindrome: %b%n", string2, isPalindrome(string2));
+        System.out.printf("\"%s\" - is palindrome: %b%n", string3, isPalindrome(string3));
     }
-//TODO сделать - учитывать только буквы, т.е. как-то смещаться может цикл while? и вообще пополам не делить :-)
+
     public static boolean isPalindrome(String string) {
-        for (int i = 0; i <= string.length() / 2; i++) {
-            if (Character.toLowerCase(string.charAt(i)) != Character.toLowerCase(string.charAt(string.length() - i - 1))) {
+        for (int i = 0, j = string.length() - 1; i < j; i++, j--) {
+            while (i < j && !Character.isLetter(string.charAt(i))) {
+                i++;
+            }
+            while (i < j && !Character.isLetter(string.charAt(j))) {
+                j--;
+            }
+            if (Character.toLowerCase(string.charAt(i)) != Character.toLowerCase(string.charAt(j))) {
                 return false;
             }
         }
