a72d79a04732030adc5666d4ccff69dcf300db7c
DiamondSi <neighborland@outlook.com>
2017-07-24 18:24:06 +0700

    palindrome

--- src/ru/academitschool/lesson8/tasks/course/Palindrome.java
+++ src/ru/academitschool/lesson8/tasks/course/Palindrome.java
@@ -14,10 +14,10 @@ public class Palindrome {
     public static boolean isPalindrome(String string) {
         for (int i = 0, j = string.length() - 1; i < j; i++, j--) {
             while (i < j && !Character.isLetter(string.charAt(i))) {
-                i++;
+                ++i;
             }
             while (i < j && !Character.isLetter(string.charAt(j))) {
-                j--;
+                --j;
             }
             if (Character.toLowerCase(string.charAt(i)) != Character.toLowerCase(string.charAt(j))) {
                 return false;
