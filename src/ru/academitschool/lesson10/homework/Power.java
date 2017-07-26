8415afb5fab7128e2096a706ff1da12d59cb5523
DiamondSi <neighborland@outlook.com>
2017-07-26 07:18:51 +0700

    get array average

---
+++ src/ru/academitschool/lesson10/homework/Power.java
@@ -0,0 +1,27 @@
+package ru.academitschool.lesson10.homework;
+
+// 10.22
+public class Power {
+    public static void main(String[] args) {
+        int number = 2;
+        int power = 10;
+        System.out.printf("%d ^ %d = %d%n", number, power, calculatePower(number, power));
+        System.out.printf("%d ^ %d = %d%n", number, power, calculatePowerByRecursion(number, power));
+    }
+
+    public static int calculatePower(int number, int power) {
+        int result = 1;
+        for (int i = 1; i <= power; i++) {
+            result *= number;
+        }
+        return result;
+    }
+
+    public static int calculatePowerByRecursion(int number, int power) {
+        if (power > 1) {
+            return number * calculatePowerByRecursion(number, power - 1);
+        } else if (power == 1) {
+            return number;
+        } else return 1;
+    }
+}
