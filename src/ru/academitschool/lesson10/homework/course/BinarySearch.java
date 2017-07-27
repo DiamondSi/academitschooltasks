e67d88c0ffa39c72084d338b6a76efb2493087be
DiamondSi <neighborland@outlook.com>
2017-07-27 20:19:56 +0700

    BinaryFindig started

---
+++ src/ru/academitschool/lesson10/homework/course/BinarySearch.java
@@ -0,0 +1,14 @@
+package ru.academitschool.lesson10.homework.course;
+
+// 10.33
+public class BinarySearch {
+    public static int findElemetIndex(double element, int left, int right, double array[]) {
+        if (left>right){
+            return -1;
+        }else
+        if (element > array[middle]) {
+            left = middle + 1;
+            findElemetIndex(double element, int lefr, int right, double array[]);
+        }
+    }
+}
