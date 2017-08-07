cf9893d4e7487fe725665f024d5d38b97058112b
DiamondSi <neighborland@outlook.com>
2017-08-07 08:19:18 +0700

    graphics

---
+++ src/ru/academitschool/lesson13/tasks/homework/BlackWhiteConversion.java
@@ -0,0 +1,43 @@
+package ru.academitschool.lesson13.tasks.homework;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
+import java.io.File;
+import java.io.IOException;
+
+public class BlackWhiteConversion {
+    public static void main(String[] args) throws IOException {
+        // читаем картинку из файлу image.jpg в объект класса BufferedImage
+        BufferedImage image = ImageIO.read(new File("image.jpg"));
+
+        WritableRaster raster = image.getRaster();
+
+        // получаем ширину и высоту картинки
+        int width = raster.getWidth();
+        int height = raster.getHeight();
+
+        final int COLORS_COUNT_IN_RGB = 3;
+
+        // создаем массив, в котором будет содержаться текущий пиксель
+        int[] pixel = new int[COLORS_COUNT_IN_RGB];
+
+        // цикл по строкам картинки
+        for (int j = 0; j < height; ++j) {
+            // цикл пикселям строки
+            for (int i = 0; i < width; ++i) {
+                // получаем текущий пиксель с координатами i,j
+                raster.getPixel(i, j, pixel);
+
+
+                // инвертируем цвет для каждой компоненты, т.е. делаем 255 минус текущее значение
+                int color = (int) (0.3 * pixel[0] + 0.59 * pixel[1] + 0.11 * pixel[2]);
+                for (int k = 0; k < COLORS_COUNT_IN_RGB; ++k) {
+                    pixel[k] = color;
+                }
+                raster.setPixel(i, j, pixel);
+            }
+        }
+        ImageIO.write(image, "png", new File("bwout.png"));
+    }
+}
---
+++ src/ru/academitschool/lesson13/tasks/homework/Blur.java
@@ -0,0 +1,54 @@
+package ru.academitschool.lesson13.tasks.homework;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
+import java.io.File;
+import java.io.IOException;
+
+public class Blur {
+    public static void main(String[] args) throws IOException {
+        BufferedImage imageIn = ImageIO.read(new File("image.jpg"));
+        BufferedImage imageOut = ImageIO.read(new File("out.jpg"));
+
+        WritableRaster rasterIn = imageIn.getRaster();
+        WritableRaster rasterOut = imageOut.getRaster();
+
+        int width = rasterIn.getWidth();
+        int height = rasterIn.getHeight();
+
+        final int COLORS_COUNT_IN_RGB = 3;
+
+        int[] pixelTopLeft = new int[COLORS_COUNT_IN_RGB];
+        int[] pixelTop = new int[COLORS_COUNT_IN_RGB];
+        int[] pixelTopRight = new int[COLORS_COUNT_IN_RGB];
+        int[] pixelLeft = new int[COLORS_COUNT_IN_RGB];
+        int[] pixel = new int[COLORS_COUNT_IN_RGB];
+        int[] pixelRight = new int[COLORS_COUNT_IN_RGB];
+        int[] pixelBottomLeft = new int[COLORS_COUNT_IN_RGB];
+        int[] pixelBottom = new int[COLORS_COUNT_IN_RGB];
+        int[] pixelBottomRight = new int[COLORS_COUNT_IN_RGB];
+
+        for (int y = 1; y < height - 1; ++y) {
+            for (int x = 1; x < width - 1; ++x) {
+                rasterIn.getPixel(x - 1, y - 1, pixelTopLeft);
+                rasterIn.getPixel(x, y - 1, pixelTop);
+                rasterIn.getPixel(x + 1, y - 1, pixelTopRight);
+                rasterIn.getPixel(x - 1, y, pixelLeft);
+                rasterIn.getPixel(x, y, pixel);
+                rasterIn.getPixel(x + 1, y, pixelRight);
+                rasterIn.getPixel(x - 1, y + 1, pixelBottomLeft);
+                rasterIn.getPixel(x, y + 1, pixelBottom);
+                rasterIn.getPixel(x + 1, y + 1, pixelBottomRight);
+
+                for (int k = 0; k < COLORS_COUNT_IN_RGB; ++k) {
+                    pixel[k] = (int) ((pixelTopLeft[k] + pixelTop[k] + pixelTopRight[k] +
+                            pixelLeft[k] + pixel[k] + pixelRight[k] +
+                            pixelBottomLeft[k] + pixelBottom[k] + pixelBottomRight[k]) / 9.0);
+                }
+                rasterOut.setPixel(x, y, pixel);
+            }
+        }
+        ImageIO.write(imageOut, "jpg", new File("out.jpg"));
+    }
+}
