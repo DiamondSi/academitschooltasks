783c6d58b418ad907d6675065995b7f3abb6b63b
DiamondSi <neighborland@outlook.com>
2017-08-26 21:55:07 +0700

    course finished

--- src/ru/academitschool/lesson11/tasks/course/QuickSort.java
+++ src/ru/academitschool/lesson11/tasks/course/QuickSort.java
@@ -4,18 +4,54 @@ import java.util.Arrays;
 
 public class QuickSort {
     public static void main(String[] args) {
-        int[] array = {10, 1, 2, 56, 33, 4};
+        int[] array = {10, 1, 3, 2, 56, 33, 4, 55, 11};
         System.out.println(Arrays.toString(array));
-        quickSort(array,0,array.length);
+        int left = 0;
+        int right = array.length - 1;
+        quickSort(array, left, right);
         System.out.println(Arrays.toString(array));
     }
+
     static void quickSort(int[] a, int left, int right) {
-        while ()
-        if (a.length == 2 && a[0] > a[1]) {
-            int temp = a[0];
-            a[0] = a[1];
-            a[1] = temp;
+        int partLength = right - left + 1;
+
+        if (partLength == 1) {
+            return;
         }
+
+        if (partLength == 2) {
+            if (a[left] > a[right]) {
+                int temp = a[left];
+                a[left] = a[right];
+                a[right] = temp;
+            }
+            return;
+        }
+
+        int i = left;
+        int j = right;
+        int x = a[left];
+
+        while (i <= j) {
+            while (a[i] < x) {
+                ++i;
+            }
+            while (a[j] > x) {
+                --j;
+            }
+            if (i <= j) {
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+                ++i;
+                --j;
+            }
+        }
+        if (i < right) {
+            quickSort(a, i, right);
+        }
+        if (j > left) {
+            quickSort(a, left, j);
         }
     }
 }
